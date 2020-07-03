540c41bb5484bf79efe019354650af6514958578
shrimp <1732341@qq.com>
2020-07-03 14:02:50 +0800

    增加中间件支持跨域

--- routes/api.go
+++ routes/api.go
@@ -5,12 +5,13 @@ import (
 	"gin-demo/modules/response"
 	"gin-demo/controllers"
 	"gin-demo/middleware/logger"
+	"gin-demo/middleware/cors"
 )
 
 func RegisterApiRouter(router *gin.Engine) {
 	apiRouter := router.Group("api")
 	
-	apiRouter.Use(logger.SetUp())
+	apiRouter.Use(logger.SetUp(), cors.SetUp())
 	
 	apiRouter.GET("/ping", func(c *gin.Context) {
 		response.ReturnHttpJsonData(c, "pong")
